b06c2060e6d9330ba2ce0b16b25d2bc1a8fea12d
ayerest <irisiseve@gmail.com>
2019-07-21 14:07:19 -0700

    finished OO Art Gallery lab

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,27 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :gallery, :artist
 
   @@all = []
 
-  def initialize(title, price)
+  #adding in gallery and artist at initialization to keep track
+  def initialize(title, price, gallery, artist)
     @title = title
     @price = price
+    @gallery = gallery
+    @artist = artist
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    #returns an integer = to the total price if all paintings
+    self.all.map do |painting|
+      painting.price
+    end.sum
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,22 @@
 require_relative '../config/environment.rb'
 
 
+artist_one = Artist.new("Andrew", 16)
+artist_two = Artist.new("Jessica", 5)
+artist_three = Artist.new("Juan", 100)
+
+gallery_one = Gallery.new("Fs", "NYC")
+gallery_two = Gallery.new("LACMA", "LA")
+
+
+painting_one = Painting.new("Blah", 100, gallery_one, artist_one)
+painting_two = Painting.new("Hi", 10000, gallery_two, artist_three)
+painting_three = Painting.new("Test", 950, gallery_two, artist_two)
+painting_four = Painting.new("Abba", 200, gallery_one, artist_one)
+painting_five = Painting.new("Frank", 250, gallery_one, artist_one)
+painting_six = Painting.new("Yes", 200000, gallery_two, artist_three)
+painting_seven = Painting.new("No", 25, gallery_one, artist_one)
+
 
 
 binding.pry
